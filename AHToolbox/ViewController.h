f56ea4fd7da70daadac3d1782afeff4b7e4b2645
Tyler Koske <numo16@gmail.com>
2012-06-19 23:24:19 -0400

    Initial Commit

---
+++ AHToolbox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AHToolbox
+//
+//  Created by Tyler Koske on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AHToolbox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AHToolbox
+//
+//  Created by Tyler Koske on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
